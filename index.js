6cad9812a0aa36e8a90f9f6959023577ba6ac74f
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-08-08 19:35:14 -0700

    commit tree 6630e41f1c53a93b760d327e36c5df2ef534086f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+const dodger = document.querySelector('div#dodger')
+
+function moveDodgerLeft() {
+    let leftPosition = parseInt(dodger.style.left.replace("px", ""))
+    if (leftPosition > 0) {
+        dodger.style.left = `${leftPosition - 10}px`
+    }
+}
+
+function moveDodgerRight() {
+    let rightPosition = parseInt(dodger.style.left.replace("px", ""))
+    if (rightPosition < 360) {
+        dodger.style.left = `${rightPosition + 10}px`
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (event.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
